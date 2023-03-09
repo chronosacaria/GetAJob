f0a702f2f63be9206971751d29256e648a0040f1
chronosacaria <chronos.droid@gmail.com>
2023-03-09 17:15:16 -0500

    Initial Commit

---
+++ src/main/java/dev/timefall/getajob/GetAJob.java
@@ -0,0 +1,13 @@
+package dev.timefall.getajob;
+
+import net.fabricmc.api.ModInitializer;
+
+public class GetAJob implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/dev/timefall/getajob/client/GetAJobClient.java
@@ -0,0 +1,13 @@
+package dev.timefall.getajob.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GetAJobClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
